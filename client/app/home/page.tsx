ba72f9dbf1f1a3be349947e5f6e3fa07af9294d9
Geethika-Kancharla <geethikak004@gmail.com>
2024-10-30 22:38:38 +0530

    Exp checked

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -3,37 +3,41 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import CodeEditor from '../components/CodeEditor';
-import { jwtDecode } from 'jwt-decode'; // Ensure you are using the correct import for jwt-decode
+import { jwtDecode } from 'jwt-decode';
 
 interface DecodedToken {
-    id: string;
+    username: string;
+    exp: number;
 }
 
 const HomePage: React.FC = () => {
     const router = useRouter();
-    const [id, setId] = useState<string | null>(null);
+    const [username, setUsername] = useState<string | null>(null);
 
     useEffect(() => {
         const token = localStorage.getItem('token');
-
         if (!token) {
-            router.push('/'); // Redirect if no token is found
+            router.push('/');
         } else {
             try {
-                console.log(token);
-                const decoded = jwtDecode<DecodedToken>(token); // Use the correct decoding method
-                console.log("Decoded token:", decoded);
-                setId(decoded.id); // Set the user ID from the decoded token
-                console.log("User ID is:", decoded.id); // Log the ID to the console
+                const decoded = jwtDecode<DecodedToken>(token);
+                setUsername(decoded.username);
+
+                // Redirect to login if token is expired
+                if (decoded.exp * 1000 < Date.now()) {
+                    localStorage.removeItem('token');
+                    router.push('/');
+                }
             } catch (error) {
                 console.error("Error decoding JWT:", error);
+                router.push('/');
             }
         }
-    }, [router]); // Add router as a dependency
+    }, [router]);
 
     return (
         <div className="min-h-lvh bg-black px-6 py-8">
-            <CodeEditor />
+            {username && <CodeEditor username={username} />}
         </div>
     );
 };
